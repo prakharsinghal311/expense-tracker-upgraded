fe580e086114e6ef55ee5f6673d5bee53de539fc
prakharsinghal311 <prakhargd31121999@gmail.com>
2023-05-24 15:21:33 +0530

    Creating Signup Screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import SignUp from "./pages/SignUp";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 
 function App() {
-  return;
+  return (
+    <BrowserRouter>
+      <Layout />
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
